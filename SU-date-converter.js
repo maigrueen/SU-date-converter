f8520340c1a3d46ac2e4e1c16bb6a3ac10e003fb
maigrueen <astrid@maigrueen.de>
2018-11-25 16:49:20 +0100

    Add files via upload
    tried to make things work

--- SU-date-converter.js
+++ SU-date-converter.js
@@ -2,17 +2,13 @@
 
 var date = document.querySelector("#newDate"),
 starttime = document.querySelector("#newStarttime"),
-//add validation, that endtime is later than starttime
+//add validation, that endtime is bigger than starttime
 endtime = document.querySelector("#newEndtime"),
 rerun = document.querySelector("#newReRun"),
 outputString = document.querySelector("#outputString"),
 submit = document.querySelector("#btn");
 
 var convertToString = function(event) {
-  //Format als String zusammen fügen:
-  // 'R/'+date+'T'+starttime+'/+date+'endtime'+'/P'
-  // als Output Anzeigen
-  console.log(outputString);
-  outputString = 'R/'+date.value+'T'+starttime.value+':00/'+date.value+'T'+endtime.value+':00/P';
+  outputString.value = 'R/'+date.value+'T'+starttime.value+':00/'+date.value+'T'+endtime.value+':00/P0Y0M'+rerun.value+'DT0H0M';
 };
-document.addEventListener("click", convertToString);
+submit.addEventListener("click", convertToString);
